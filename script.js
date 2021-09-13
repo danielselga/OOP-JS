a50324b328f0e1bc00ddcde1b4619e75af6521c3
Daniel <danielselga@gmail.com>
2021-09-13 13:11:51 -0300

    chore: OPP Concept and Standard JS instaled

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-'use strict';
+'use strict'
+
+// CONCEPTS
+
+// OBJECT-ORIENTED-PROGRAMMING (OOP) is a programming paradigm based on the concept of objects. paradigm = style of the code.
+// We use objects to model (describe) real-world or abstract features.
+// Objects may contains data (properties) and code (methods). By Using objects, we pack data and the corresponding behavior into one block.
+// In oop, objects are self-contained pieces/blocks of code.
+// Objects are building blocks of aplications, and interact with one another.
+// Interactions happens through a public interface (API): methods that the code outside of the object can access and use to comunicate with the project.
